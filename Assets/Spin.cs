b45ffd0f021e7260ef665c343fbd055b34c2b8c4
Maelse <niedzwiedzmaciej4@gmail.com>
2020-03-27 19:37:43 +0000

    Slot machine project

---
+++ Assets/Spin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void spinno()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            if (transform.position.y >= 4)
+                transform.position = new Vector2(transform.position.x, -5.3f);
+
+
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1.5f);
+
+            
+        }
+    }
+
+}
---
+++ Assets/onPressSpin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onPressSpin : MonoBehaviour
+{
+    public GameObject row;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator pressSpin()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            if (row.transform.position.y >= 4)
+                row.transform.position = new Vector2(row.transform.position.x, -5.3f);
+
+
+            row.gameObject.transform.position = new Vector2(row.gameObject.transform.position.x, row.gameObject.transform.position.y + 1.5f);
+
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    public void elo()
+    {
+        StartCoroutine("pressSpin");
+    }
+}
