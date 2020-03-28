33fb1fd210e8958406ad7070ad14cd3595409cc9
Maelse <niedzwiedzmaciej4@gmail.com>
2020-03-28 20:15:04 +0000

    Add Rows

--- Assets/onPressSpin.cs
+++ Assets/onPressSpin.cs
@@ -1,37 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class onPressSpin : MonoBehaviour
-{
-    public GameObject row;
-    // Start is called before the first frame update
-    void Start()
-    {
+{   
+    public float delayStart;
+    float m_LastPressTime;
+    float m_PressDelay = 3f;
 
+    public void delayed()
+    {
+        if (m_LastPressTime + m_PressDelay > Time.unscaledTime)
+            return;
+        m_LastPressTime = Time.unscaledTime;
+        Invoke("elo", delayStart);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     IEnumerator pressSpin()
     {
-        for (int i = 0; i < 30; i++)
         {
-            if (row.transform.position.y >= 4)
-                row.transform.position = new Vector2(row.transform.position.x, -5.3f);
+            int randomSpins = Random.Range(40, 50);
 
-
-            row.gameObject.transform.position = new Vector2(row.gameObject.transform.position.x, row.gameObject.transform.position.y + 1.5f);
-
-            yield return new WaitForSeconds(.5f);
-        }
+            if (randomSpins % 2 != 0)
+            {
+                randomSpins += 1;
+            }
+            for (int i = 0; i < randomSpins; i++)
+            {
+                if (gameObject.transform.position.y <= -14)
+                    gameObject.transform.position = new Vector2(gameObject.transform.position.x, 16.8f);
+                gameObject.gameObject.transform.position = new Vector2(gameObject.gameObject.transform.position.x, gameObject.gameObject.transform.position.y - 2.25f);
+                yield return new WaitForSeconds(0.04f);
+            }
+        }      
     }
 
     public void elo()
-    {
+    {    
         StartCoroutine("pressSpin");
+
     }
 }
